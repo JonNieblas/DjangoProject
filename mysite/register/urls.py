9acd2391ac0c0195db86c17d7453148832c2d874
JonNieblas <jniebla1@msudenver.edu>
2017-11-04 17:40:56 -0600

    Basic views created for Course, Professor, and Section. Each one displays all information related to itself, such as section names, professor names, etc.

--- mysite/register/urls.py
+++ mysite/register/urls.py
@@ -4,4 +4,20 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+
+    #ex: /register/section
+    url(r'^section/$', views.section, name='section_page'),
+    #ex: /register/section/1
+    url(r'^section/(?P<section_id>[0-9]+)/$', views.section_detail, name ='section_detail'),
+
+    #ex: /register/course
+    url(r'^course/$', views.course, name='course_page'),
+    # ex: /register/course/1
+    url(r'^course/(?P<course_id>[0-9]+)/$', views.course_detail, name='course_detail'),
+
+    #ex: /register/professor
+    url(r'^professor/$', views.professor, name='professor_page'),
+    # ex: /register/professor/1
+    url(r'^professor/(?P<professor_id>[0-9]+)/$', views.professor_detail, name='professor_detail'),
+
 ]
--- mysite/register/views.py
+++ mysite/register/views.py
@@ -1,4 +1,43 @@
 from django.http import HttpResponse
+from .models import Section
+from .models import Course
+from .models import Professor
+
+from django.shortcuts import get_object_or_404, render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the register index.")
+    html = "<h1>Welcome!</h1>" + '<br><strong><a href="/register/section/">Sections</a></strong>' + '<br><strong><a href="/register/professor/">Professors</a></strong>' + '<br><strong><a href="/register/course/">Courses</a></strong>'
+    return HttpResponse(html)
+
+def section(request):
+    section_list = Section.objects.all()
+    context = {
+        'section_list': section_list,
+    }
+    return render(request, 'register/section.html', context)
+
+def section_detail(request, section_id):
+    section = get_object_or_404(Section, pk=section_id)
+    return render(request, 'register/section_detail.html', {'section': section})
+
+def course(request):
+    course_list = Course.objects.all()
+    context = {
+        'course_list': course_list,
+    }
+    return render(request, 'register/course.html', context)
+
+def course_detail(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'register/course_detail.html', {'course': course})
+
+def professor(request):
+    professor_list = Professor.objects.all()
+    context = {
+        'professor_list': professor_list,
+    }
+    return render(request, 'register/professor.html', context)
+
+def professor_detail(request, professor_id):
+    professor = get_object_or_404(Professor, pk=professor_id)
+    return render(request, 'register/professor_detail.html', {'professor': professor})
